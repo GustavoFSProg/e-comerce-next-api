0cae9377f30c2f3338ed47594862bf9e867f13d7
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2024-07-29 19:20:14 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const {PORT} = process.env
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(PORT, () => {
+    return console.log( ` 🍌 Running App: ${PORT}`)
+})
+
+export default app
