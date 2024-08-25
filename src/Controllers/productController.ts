df6bdded85d73c0de63722fea6dbf466b400cb05
GustavoFSProg <gustavo.prog40@gmail.com>
2024-08-25 09:42:49 -0300

    multiupload pronto

--- src/Controllers/productController.ts
+++ src/Controllers/productController.ts
@@ -80,7 +80,7 @@ export async function create(req: Request, res: Response) {
         preco: Number(req.body.preco)  ,   
         descricao : req.body.descricao,
         quantity: Number(req.body.quantity),   
-                  subtotal:  Number(req.body.quantity),
+                  subtotal:  Number(req.body.subtotal),
                   imagem: "Imagem - Exemplo"  
       },
     })
