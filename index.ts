c95f73fd5b516a8031f57475863f4532ccb9e2bf
GustavoFSProg <gustavo.prog40@gmail.com>
2024-08-25 10:23:06 -0300

    web url

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "https://ecomerce-curso.netlify.app"]
 }))
 app.use(express.json())
 app.use(routes)
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const {PORT} = process.env
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: ["http://localhost:3000", "https://ecomerce-curso.netlify.app"]
 }))
 app.use(express.json())
 app.use(routes)
